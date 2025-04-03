98556d8f5684b07ed9cff63e4f2fdb82fcb7507c
Vinay Kore <vinaykore.vk@gmail.com>
2025-04-03 23:08:23 +0530

    Latest changes

--- first.js
+++ first.js
@@ -4,16 +4,13 @@ console.log(button)
 const login = "Login Page Added"
 console.log(login)
 
-<<<<<<< HEAD
 const footer ="footer added in our website"
 console.log(footer)
-=======
 const Payment = "intergrated the payment gateway"
 console.log(Payment)
-<<<<<<< HEAD
->>>>>>> 87c76f9 (Payment added)
-=======
 
 const upi = "integrated the upi"
 console.log(upi)
->>>>>>> cf42f88 (Upi added)
+
+console.log("latest update");
+
