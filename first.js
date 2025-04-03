90bfb599b25ab6bb5be15ca7aa7b02945c624ca3
Vinay Kore <vinaykore.vk@gmail.com>
2025-04-03 17:17:22 +0530

    Payment updated after footer

--- first.js
+++ first.js
@@ -4,5 +4,10 @@ console.log(button)
 const login = "Login Page Added"
 console.log(login)
 
+<<<<<<< HEAD
 const footer ="footer added in our website"
 console.log(footer)
+=======
+const Payment = "intergrated the payment gateway"
+console.log(Payment)
+>>>>>>> 87c76f9 (Payment added)
